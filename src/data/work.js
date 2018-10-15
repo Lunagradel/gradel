9a314f498e0c294b25913849b76b11e3d333f791
lunagradel <lunig@hotmail.com>
2018-10-15 22:24:17 +0200

    added data file.

---
+++ src/data/work.js
@@ -0,0 +1,54 @@
+export default [
+  {
+    thumbName: 'bachelor',
+    name: 'anaxi',
+    backgroundColor: '#F49575',
+    textColor: '#ffffff',
+    btnColor: '#ffffff',
+    images: {
+      header: 'anaxi_header.png',
+      imgOne: 'anaxi_img_1.png',
+      imgTwo: 'anaxi_img_2.gif'
+    },
+    text: '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ultricies ligula pharetra ligula tincidunt venenatis. Sed at ornare sem. Cras faucibus porta quam, ut gravida neque fringilla sit amet. Curabitur fringilla nisl dui, quis accumsan leo tempus luctus. Nulla vulputate sapien ac viverra hendrerit. Aliquam id consequat odio, ut lacinia dolor. Praesent suscipit cursus faucibus. Proin scelerisque vel magna quis ultricies.</p><p>Aliquam consequat tempus porttitor. Pellentesque maximus nisi orci. Mauris efficitur, eros eget euismod cursus, eros felis tempus odio, nec convallis ex massa in tellus. Morbi convallis, lorem sit amet ultrices condimentum, diam odio fermentum mi, sed iaculis quam est.</p>'
+  },
+  {
+    thumbName: 'join',
+    name: 'anaxi',
+    backgroundColor: '#F49575',
+    textColor: '#ffffff',
+    btnColor: '#ffffff',
+    images: {
+      header: 'anaxi_header.png',
+      imgOne: 'anaxi_img_1.png',
+      imgTwo: 'anaxi_img_2.gif'
+    },
+    text: '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ultricies ligula pharetra ligula tincidunt venenatis. Sed at ornare sem. Cras faucibus porta quam, ut gravida neque fringilla sit amet. Curabitur fringilla nisl dui, quis accumsan leo tempus luctus. Nulla vulputate sapien ac viverra hendrerit. Aliquam id consequat odio, ut lacinia dolor. Praesent suscipit cursus faucibus. Proin scelerisque vel magna quis ultricies.</p><p>Aliquam consequat tempus porttitor. Pellentesque maximus nisi orci. Mauris efficitur, eros eget euismod cursus, eros felis tempus odio, nec convallis ex massa in tellus. Morbi convallis, lorem sit amet ultrices condimentum, diam odio fermentum mi, sed iaculis quam est.</p>'
+  },
+  {
+    thumbName: 'graphic design',
+    name: 'anaxi',
+    backgroundColor: '#F49575',
+    textColor: '#ffffff',
+    btnColor: '#ffffff',
+    images: {
+      header: 'anaxi_header.png',
+      imgOne: 'anaxi_img_1.png',
+      imgTwo: 'anaxi_img_2.gif'
+    },
+    text: '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ultricies ligula pharetra ligula tincidunt venenatis. Sed at ornare sem. Cras faucibus porta quam, ut gravida neque fringilla sit amet. Curabitur fringilla nisl dui, quis accumsan leo tempus luctus. Nulla vulputate sapien ac viverra hendrerit. Aliquam id consequat odio, ut lacinia dolor. Praesent suscipit cursus faucibus. Proin scelerisque vel magna quis ultricies.</p><p>Aliquam consequat tempus porttitor. Pellentesque maximus nisi orci. Mauris efficitur, eros eget euismod cursus, eros felis tempus odio, nec convallis ex massa in tellus. Morbi convallis, lorem sit amet ultrices condimentum, diam odio fermentum mi, sed iaculis quam est.</p>'
+  },
+  {
+    thumbName: 'game',
+    name: 'anaxi',
+    backgroundColor: '#F49575',
+    textColor: '#ffffff',
+    btnColor: '#ffffff',
+    images: {
+      header: 'anaxi_header.png',
+      imgOne: 'anaxi_img_1.png',
+      imgTwo: 'anaxi_img_2.gif'
+    },
+    text: '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ultricies ligula pharetra ligula tincidunt venenatis. Sed at ornare sem. Cras faucibus porta quam, ut gravida neque fringilla sit amet. Curabitur fringilla nisl dui, quis accumsan leo tempus luctus. Nulla vulputate sapien ac viverra hendrerit. Aliquam id consequat odio, ut lacinia dolor. Praesent suscipit cursus faucibus. Proin scelerisque vel magna quis ultricies.</p><p>Aliquam consequat tempus porttitor. Pellentesque maximus nisi orci. Mauris efficitur, eros eget euismod cursus, eros felis tempus odio, nec convallis ex massa in tellus. Morbi convallis, lorem sit amet ultrices condimentum, diam odio fermentum mi, sed iaculis quam est.</p>'
+  }
+]
