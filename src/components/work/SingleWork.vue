7b904b8126c849819a1a0c28a45f6f0e56324992
lunagradel <lunig@hotmail.com>
2018-10-15 22:24:01 +0200

    added work and singlework files and styled them.

---
+++ src/components/work/SingleWork.vue
@@ -0,0 +1,42 @@
+<template>
+  <div :class="['singlework-wrapper', {'go-down': close}]" v-bind:style="{ backgroundColor: chosen.backgroundColor }">
+    <div class="sw-top" v-bind:style="{ backgroundImage: 'url(' + require('./../../assets/' + chosen.images.header) + ')' }">
+      <div class="close" :style="{color: chosen.btnColor}" v-on:click="handleClose">&#10006;</div>
+      <div class="gradient-layer" v-bind:style="{ background: ['linear-gradient(to bottom, transparent 60%,'+ chosen.backgroundColor +' 100%)', '-webkit-linear-gradient(top, transparent 60%, '+chosen.backgroundColor+' 100%)', '-moz-linear-gradient(top, transparent 60%, '+chosen.backgroundColor+' 100%)']}"></div>
+    </div>
+    <div class="sw-content" v-bind:style="{ backgroundColor: chosen.backgroundColor }">
+      <div class="sw-images">
+        <div class="sw-img">
+          <img :src="require('./../../assets/'+ chosen.images.imgOne)" alt="">
+        </div>
+        <div class="sw-img">
+          <img :src="require('./../../assets/'+ chosen.images.imgTwo)" alt="">
+        </div>
+      </div>
+      <div class="sw-text" :style="{ color: chosen.textColor }">
+        <span v-html="chosen.text"></span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'SingleWork',
+  props: ['chosen'],
+  data: () => ({
+    close: false
+  }),
+  methods: {
+    handleClose(){
+      this.close = true;
+      this.$emit('startAnimate');
+      let self = this;
+      setTimeout(function(){
+        self.$emit('closeSingle');
+      }, 900);
+    }
+  }
+}
+</script>
---
+++ src/views/Work.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="work-wrapper page-wrapper">
+    <div class="header-container">
+      <p :class="['header page-title', {remove: animate, add: !animate}]">WORK</p>
+      <p :class="['header work-title', {add: animate, remove: !animate}]" v-if="showSingleWork">{{chosenWork.thumbName}}</p>
+    </div>
+    <div class="work-container">
+      <!-- <div v-for="(item, index) in workData" :key="index" :class="['work-element', {enlarge: index == selected, test: index != selected && selected != null }]" v-on:click="handleClick(index)"> -->
+      <div :class="['work-elements', {remove: animate}]">
+        <div v-for="(item, index) in workData" :key="index" class="work-element" v-on:click="handleClick(index)" v-bind:style="{ backgroundImage: 'url(' + require('./../assets/' + item.images.header) + ')' }">
+          <div class="element-overlay">
+          </div>
+          <p class="header">{{item.thumbName}}</p>
+        </div>
+      </div>
+      <SingleWork v-if="showSingleWork" v-bind:chosen="chosenWork" @closeSingle="handleShowSW" @startAnimate="animate = false"/>
+    </div>
+
+  </div>
+</template>
+
+<script>
+import workData from './../data/work.js';
+import SingleWork from './../components/work/SingleWork.vue';
+
+export default {
+  name: 'work',
+  data: () => ({
+    workData,
+    chosenWork: null,
+    showSingleWork: false,
+    animate: false
+  }),
+  methods: {
+    handleClick(index){
+      this.selected = index;
+      this.chosenWork = this.workData[index];
+      this.animate = true;
+      this.handleShowSW();
+    },
+    handleShowSW(){
+      this.showSingleWork = !this.showSingleWork;
+    }
+  },
+  components: {
+    SingleWork
+  }
+}
+</script>
